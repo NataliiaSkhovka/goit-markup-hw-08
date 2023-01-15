de9ef91aa127a905ab5f73a33e46e4ea2dc35ed2
NataliiaSkhovka <106191863+NataliiaSkhovka@users.noreply.github.com>
2023-01-15 12:48:24 +0000

    1248150123

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
-  const body = document.querySelector("[data-body]");
+  const body = document.querySelector("body");
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
